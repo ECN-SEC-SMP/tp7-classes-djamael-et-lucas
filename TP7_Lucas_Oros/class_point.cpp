d93bacd631f30d7e384a08770a9f7899bef28f59
Lucas_Oros <oroslucas1604@gmail.com>
2025-01-31 10:15:07 +0100

    partie 1 dans class_point.cpp

---
+++ TP7_Lucas_Oros/class_point.cpp
@@ -0,0 +1,51 @@
+#include "class_point.h"
+#include <iostream>
+
+using namespace std;
+
+// Constructeur par défaut
+point::point(){
+    this->x = 0;
+    this->y = 0;
+}
+
+// Constructeur avec paramètres
+point::point(int x, int y) : x(x), y(y) {}
+
+// Constructeur de recopie
+point::point(const point &p) : x(p.x), y(p.y) {}
+
+// Getters
+int point::get_x() {
+    return x;
+}
+
+int point::get_y() {
+    return y;
+}
+
+// Setters
+void point::set_x(int x) {
+    this->x = x;
+}
+
+void point::set_y(int y) {
+    this->y = y;
+}
+
+// Translation avec deux réels
+void point::translater(int x_t, int y_t) {
+    this->x = x_t + x;
+    this->y = y_t + y;
+}
+
+// Translation avec un autre point
+void point::translater(const point &p) {
+    x += p.x;
+    y += p.y;
+}
+
+// Affichage des coordonnées avvec get_x et get_y
+void point::afficher() {
+    cout << "x = " << get_x() << " y = " << get_y() << endl;
+}
